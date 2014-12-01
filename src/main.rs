9d555dc4e6d563b34d66a89c3be272b3950579e4
Bryce Fisher-Fleig <bryce@fisher-fleig.org>
2014-11-30 18:43:55 -0800

    (feat) trivial server impl
    TOTALLY TRIVIAL
    
    This literally runs AFTER the dummy transcode implementation. I'm
    just hacking away here, trying to combine the hyper server with
    the hyper client / pistondevelopers/image transcode piece I already
    have.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
+#![feature(macro_rules, phase)]
+
 extern crate hyper;
 extern crate mime;
 extern crate image;
 
 use hyper::Url;
-use hyper::client::Request;
-use hyper::header::common::ContentType;
+use hyper::client;
+use hyper::header::common::{ContentType, ContentLength};
+use hyper::Get;
+use hyper::server::{Server, Request, Response};
+
 use mime::{Mime, Image, Jpeg, Png, Gif};
+
+#[allow(unused_imports)]
 use image::{load_from_memory, GenericImage, DynamicImage};
+
 use std::io::File;
 use std::error::Error;
+use std::io::net::ip::Ipv4Addr;
+
 use TranscodeError::{HttpStatusError, UnsupportedContentType, HttpBodyError, PistonImageLoadError, RemoteServerUnreachable};
+
+macro_rules! try_return(
+    ($e:expr) => {{
+        match $e {
+            Ok(v) => v,
+            Err(e) => { println!("Error: {}", e); return; }
+        }
+    }}
+)
 
 #[deriving(Show, PartialEq, Clone)]
 pub enum TranscodeError {
@@ -35,7 +54,7 @@ impl Error for TranscodeError {
 type TranscodeResult<T> = Result<T, TranscodeError>;
 
 fn load_img_from_url(url: Url) -> TranscodeResult<DynamicImage> {
-    let req = match Request::get(url) {
+    let req = match client::Request::get(url) {
         Ok(req) => req,
         Err(_) => return Err(RemoteServerUnreachable)
     };
@@ -62,8 +81,35 @@ fn load_img_from_url(url: Url) -> TranscodeResult<DynamicImage> {
     }
 }
 
+#[allow(unused_mut)]
+fn parse_request(mut req: Request, mut res: Response) {
+    match req.uri {
+        hyper::uri::AbsolutePath(ref path) => match (&req.method, path.as_slice()) {
+            (&Get, "/") | (&Get, "/echo") => {
+                let out = b"Try POST /echo";
+                res.headers_mut().set(ContentLength(out.len()));
+                let mut res = try_return!(res.start());
+                try_return!(res.write(out));
+                try_return!(res.end());
+                return;
+            },
+            _ => {
+                *res.status_mut() = hyper::status::NotFound;
+                try_return!(res.start().and_then(|res| res.end()));
+                return;
+            }
+        },
+        _ => {
+            try_return!(res.start().and_then(|res| res.end()));
+            return;
+        }
+    };
+}
+
 fn main() {
-    let url = match Url::parse("http://c2.staticflickr.com/8/7384/12315308103_94b0a3f6cd_c.jpg") {
+    // Transcode
+    //let url = match Url::parse("http://c2.staticflickr.com/8/7384/12315308103_94b0a3f6cd_c.jpg") {
+    let url = match Url::parse("http://c2.staticflickr.com/6/5145/5548591309_b0c26f6b47_b.jpg") {
         Ok(url) => {
             println!("GET {}...", url)
             url
@@ -72,10 +118,16 @@ fn main() {
     };
 
     let img_in = match load_img_from_url(url) {
-        Ok(img) => img,
+        Ok(img) => { println!("Successfully loaded image into memory"); img },
         Err(e) => panic!("Error loading img from url {}", e)
     };
 
     let fout = File::create(&Path::new("test.png")).unwrap();
     let _ = img_in.save(fout, image::PNG);
+    println!("Saved image to disk as a png");
+
+    // Server
+    let server = Server::http(Ipv4Addr(127, 0, 0, 1), 1337);
+    server.listen(parse_request).unwrap();
+    println!("Listening on http://127.0.0.1:1337");
 }
